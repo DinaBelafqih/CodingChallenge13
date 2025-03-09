0639cc74d1a0dc0d967aaea1bc37183083bfba75
dinabelafqih <belafqihdina@gmail.com>
2025-03-09 18:58:53 -0400

    Task 2 - Employee Cards Dynamic Addition

---
+++ cc_13.js
@@ -0,0 +1,10 @@
+// Task 2 - Employee Cards Dynamic Addition
+function addEmployeeCard(name, position) {
+    // main employee card container
+    const card = document.createElement('div');
+    card.setAttribute('class', 'employee-card'); // class for style
+
+    const heading = document.createElement('h3'); // name as heading
+    heading.textContent = name;
+
+    const paragraph = document.createElement('h3'); // position is a paragragh
